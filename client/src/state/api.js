d66bb1e2c5053e7606999ffcbf2dd2332f157abb
Willi <nardowilli@gmail.com>
2022-12-21 10:29:08 +0700

    fix bug env local

--- client/src/state/api.js
+++ client/src/state/api.js
@@ -1,14 +1,14 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const api = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5001/client/qahoaxnews" }),
+  baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_BASE_URL }),
   reducerPath: "adminApi",
     tagTypes: [
         "Hoax"
     ],
     endpoints:(build) => ({
         getQAHoaxNews: build.query({
-            query: () => "",
+            query: () => "client/qahoaxnews",
             providesTags: ["Hoax"],
         })
     })
